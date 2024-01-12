c90f9d30f5d414012db744bd6b45a450b9fa78d3
abvbobko <abvbobko@gmail.com>
2024-01-13 01:18:38 +0300

    fix frames and bug with colors

--- src/utils.py
+++ src/utils.py
@@ -7,8 +7,9 @@ from src.enums import Color
 
 def color_to_number(color: Color) -> int:
     color = ImageColor.getrgb(color.value)
+    r, g, b = color
 
-    return ImagePalette.ImagePalette(mode="RGB").getcolor(color)
+    return (r * 256 * 256) + (g * 256) + b
 
 
 def get_image_name(image: Image.Image) -> str:
